385e1e661fa7f831e79eaa8a65fd58428bbc1730
Henry Kang <hgang1012@gmail.com>
2022-03-01 15:41:11 -0800

    Initial Files

---
+++ src/test/java/com/itlize/jooleproject/JooleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itlize.jooleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooleprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
