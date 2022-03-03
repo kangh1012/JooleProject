2fc6be14d32e0bed0fc3fbcb3b6812396c71f956
Henry Kang <hgang1012@gmail.com>
2022-03-02 17:26:45 -0800

    Created Entity Classes

---
+++ src/main/java/com/itlize/jooleproject/entity/Product.java
@@ -0,0 +1,92 @@
+package com.itlize.jooleproject.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long productId;
+
+    private String type;
+    private String manufacturer;
+    private String model;
+    private Integer modelYear;
+    private LocalDateTime timeCreated;
+    private LocalDateTime lastModified;
+
+    @OneToMany(mappedBy = "product")
+    private Set<ProjectResource> resources;
+
+    public Product() {
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(Integer modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public LocalDateTime getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalDateTime timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public LocalDateTime getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(LocalDateTime lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public Set<ProjectResource> getResources() {
+        return resources;
+    }
+
+    public void setResources(Set<ProjectResource> resources) {
+        this.resources = resources;
+    }
+}
