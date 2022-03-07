57aab70f4bdec1df8783560dad5174a5559a2c79
Yiqian Li <nyankapoi@gmail.com>
2022-03-06 20:18:07 -0500

    userController update

---
+++ src/main/java/com/itlize/jooleproject/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.itlize.jooleproject.controller;
+
+import com.itlize.jooleproject.entity.User;
+import com.itlize.jooleproject.service.UserService;
+import com.itlize.jooleproject.util.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("register")
+    public JsonResult<Void> register(User user){
+        LocalDateTime date = LocalDateTime.now();
+        user.setTimeCreated(date);
+        user.setLastModified(date);
+
+        JsonResult<Void> result = new JsonResult<>();
+        if (userService.findByUsername(user.getUsername()) != null){
+            result.setState(4000);
+            result.setMessage("User already exist.");
+        }else try{
+            userService.save(user);
+            result.setState(200);
+            result.setMessage("Successfully registered.");
+        }catch(Exception e){
+            result.setState(4000);
+            result.setMessage("Error happens when trying to register");
+        }
+
+        return result;
+    }
+
+
+
+}
